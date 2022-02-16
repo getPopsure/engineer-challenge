4816f3cbebf3695c953e03f4a78a778569a620cc
Darionti88 <darionti@gmail.com>
2022-02-16 19:16:00 -0300

    Add color palette to Tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,16 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        featherPurple: "#8e8cee",
+        featherDarkPurple: "#5f5e9e",
+        featherGreen: "#90df9c",
+        featherOrange: "#e59254",
+        featherPink: "#ee5f92",
+        text: "#26262f",
+      },
+    },
   },
   variants: {
     extend: {},
