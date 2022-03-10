b9d88bfc4d8f41c7a53c355d72197c95c2b4e6a6
Luppov Artur <luppoffart@icloud.com>
2022-03-11 01:45:48 +0500

    Make monorepo codebase structure.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,11 @@
 import "./index.css";
 
 function App() {
-  return <h1 className="text-3xl">Fullstack code challenge</h1>;
+  return (
+    <>
+      <h1 className="text-3xl">Fullstack code challenge</h1>
+    </>
+  );
 }
 
 export default App;
