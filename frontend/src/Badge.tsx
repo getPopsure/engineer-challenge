2636091e9e8c0d6d335322262657ea32a096b81b
Ricardo Velasco <talo242@gmail.com>
2022-04-06 15:03:10 +0200

    feat: add frontend boilerplate

---
+++ frontend/src/Badge.tsx
@@ -0,0 +1,28 @@
+type Status = 'ACTIVE' | 'PENDING' | 'CANCELLED' | 'DROPPED_OUT'
+
+interface BadgeProps {
+  status: Status
+}
+
+const getBadgeColors = (status: Status) => {
+  const badgeColors: { [k in Status]: {
+    textColor: string,
+    backgroundColor: string
+  } } = {
+    ACTIVE: { textColor: 'tc-green-100', backgroundColor: 'bg-green-100' },
+    PENDING: { textColor: 'tc-yellow-600', backgroundColor: 'bg-yellow-100' },
+    CANCELLED: { textColor: 'tc-red-100', backgroundColor: 'bg-red-100' },
+    DROPPED_OUT: { textColor: 'tc-red-100', backgroundColor: 'bg-red-100' },
+  }
+
+  return badgeColors[status]
+}
+
+const Badge = ({ status }: BadgeProps) => {
+  const { textColor, backgroundColor } = getBadgeColors(status);
+  const className = `inline-block rounded-full	py-1 px-4 font-semibold text-xs ${textColor} ${backgroundColor}`;
+
+  return <p className={className}>{status}</p>
+}
+
+export default Badge;
