ade33d60f74b764ac6017b280b25f9f2196fc3f7
ruizhen88 <yejiin.jung@gmail.com>
2022-04-14 16:00:23 +0800

    feat: filter insuranceType and status by query string

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,10 @@
 import express from "express";
-import { PrismaClient, Prisma } from "@prisma/client";
+import {
+  PrismaClient,
+  Prisma,
+  InsuranceType,
+  PolicyStatus,
+} from "@prisma/client";
 import cors from "cors";
 
 const app = express();
@@ -8,6 +13,17 @@ const prisma = new PrismaClient();
 
 app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.json());
+
+const stringToEnumValue = <T>(
+  enm: { [s: string]: T },
+  value: string
+): T[] | undefined => {
+  const filtered = Object.keys(enm).filter((item) => {
+    return item.includes(value.toUpperCase());
+  });
+  const mapped = filtered.map((item) => enm[item]); // convert to enum value type
+  return mapped.length ? mapped : undefined;
+};
 
 app.get("/policies", async (req, res) => {
   const { search } = req.query;
@@ -24,6 +40,16 @@ app.get("/policies", async (req, res) => {
           {
             customer: {
               lastName: { contains: search as string, mode: "insensitive" },
+            },
+          },
+          {
+            insuranceType: {
+              in: stringToEnumValue(InsuranceType, search as string),
+            },
+          },
+          {
+            status: {
+              in: stringToEnumValue(PolicyStatus, search as string),
             },
           },
         ],
