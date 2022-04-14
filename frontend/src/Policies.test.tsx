077a11d24a40d9452f6fcaa768e0daf40bb6e2ff
ruizhen88 <yejiin.jung@gmail.com>
2022-04-14 12:22:27 +0800

    test: count matches on initial render

--- frontend/src/Policies.test.tsx
+++ frontend/src/Policies.test.tsx
@@ -9,9 +9,9 @@ import { rest } from "msw";
 import { setupServer } from "msw/node";
 import Policies from "./Policies";
 
-// taken from seed.ts
 const mockData = [
   {
+    id: "1",
     customer: {
       firstName: "Cyrillus",
       lastName: "Biddlecombe",
@@ -24,6 +24,7 @@ const mockData = [
     // createdAt
   },
   {
+    id: "2",
     customer: {
       firstName: "Brandy",
       lastName: "Harbour",
@@ -34,6 +35,7 @@ const mockData = [
     startDate: "2015-01-13T04:52:15Z",
   },
   {
+    id: "3",
     customer: {
       firstName: "Ailina",
       lastName: "Harber",
@@ -44,6 +46,7 @@ const mockData = [
     startDate: "2014-07-14T00:54:34Z",
   },
   {
+    id: "4",
     customer: {
       firstName: "Aguste",
       lastName: "Bilsford",
@@ -54,6 +57,7 @@ const mockData = [
     startDate: "2020-07-21T19:40:35Z",
   },
   {
+    id: "5",
     customer: {
       firstName: "Haydon",
       lastName: "Ballay",
@@ -64,6 +68,7 @@ const mockData = [
     startDate: "2013-03-30T19:27:54Z",
   },
   {
+    id: "6",
     customer: {
       firstName: "Brandyn",
       lastName: "Argyle",
@@ -74,6 +79,7 @@ const mockData = [
     startDate: "2021-01-12T19:24:41Z",
   },
   {
+    id: "7",
     customer: {
       firstName: "Tani",
       lastName: "Erasmus",
@@ -84,6 +90,7 @@ const mockData = [
     startDate: "2018-05-11T11:56:51Z",
   },
   {
+    id: "8",
     customer: {
       firstName: "Galvan",
       lastName: "Suggey",
@@ -94,6 +101,7 @@ const mockData = [
     startDate: "2013-01-25T04:14:34Z",
   },
   {
+    id: "9",
     customer: {
       firstName: "Rozelle",
       lastName: "Nipper",
@@ -104,6 +112,7 @@ const mockData = [
     startDate: "2012-09-24T09:55:17Z",
   },
   {
+    id: "10",
     customer: {
       firstName: "Flossie",
       lastName: "Camings",
@@ -113,126 +122,62 @@ const mockData = [
     status: "PENDING",
     startDate: "2020-05-02T05:53:46Z",
   },
+];
+
+const mockDataAok = [
   {
+    id: "3",
     customer: {
-      firstName: "Derril",
-      lastName: "Gildersleeve",
-    },
-    provider: "BARMER",
-    insuranceType: "HOUSEHOLD",
-    status: "PENDING",
-    startDate: "2022-01-12T17:47:41Z",
-  },
-  {
-    customer: {
-      firstName: "Amanda",
-      lastName: "McPherson",
-    },
-    provider: "TK",
-    insuranceType: "HEALTH",
-    status: "ACTIVE",
-    startDate: "2012-07-04T15:31:29Z",
-  },
-  {
-    customer: {
-      firstName: "Garnette",
-      lastName: "Benda",
-    },
-    provider: "TK",
-    insuranceType: "HEALTH",
-    status: "PENDING",
-    startDate: "2012-08-01T03:25:59Z",
-  },
-  {
-    customer: {
-      firstName: "Yoko",
-      lastName: "Becker",
-    },
-    provider: "DAK",
-    insuranceType: "LIABILITY",
-    status: "DROPPED_OUT",
-    startDate: "2014-02-22T06:02:58Z",
-  },
-  {
-    customer: {
-      firstName: "Sam",
-      lastName: "Penni",
-    },
-    provider: "BARMER",
-    insuranceType: "LIABILITY",
-    status: "ACTIVE",
-    startDate: "2014-04-14T12:39:02Z",
-  },
-  {
-    customer: {
-      firstName: "Jeffie",
-      lastName: "Pinyon",
-    },
-    provider: "TK",
-    insuranceType: "HOUSEHOLD",
-    status: "DROPPED_OUT",
-    startDate: "2017-05-28T11:56:27Z",
-  },
-  {
-    customer: {
-      firstName: "Mariette",
-      lastName: "Cristofanini",
+      firstName: "Ailina",
+      lastName: "Harber",
     },
     provider: "AOK",
-    insuranceType: "HOUSEHOLD",
+    insuranceType: "HEALTH",
+    status: "DROPPED_OUT",
+    startDate: "2014-07-14T00:54:34Z",
+  },
+  {
+    id: "4",
+    customer: {
+      firstName: "Aguste",
+      lastName: "Bilsford",
+    },
+    provider: "AOK",
+    insuranceType: "HEALTH",
+    status: "PENDING",
+    startDate: "2020-07-21T19:40:35Z",
+  },
+  {
+    id: "6",
+    customer: {
+      firstName: "Brandyn",
+      lastName: "Argyle",
+    },
+    provider: "AOK",
+    insuranceType: "HEALTH",
     status: "CANCELLED",
-    startDate: "2012-03-29T01:59:13Z",
+    startDate: "2021-01-12T19:24:41Z",
   },
   {
+    id: "10",
     customer: {
-      firstName: "Jess",
-      lastName: "Whittle",
-    },
-    provider: "DAK",
-    insuranceType: "LIABILITY",
-    status: "PENDING",
-    startDate: "2013-11-15T09:58:45Z",
-  },
-  {
-    customer: {
-      firstName: "Graeme",
-      lastName: "Ternent",
-    },
-    provider: "DAK",
-    insuranceType: "HEALTH",
-    status: "ACTIVE",
-    startDate: "2020-08-16T03:24:30Z",
-  },
-  {
-    customer: {
-      firstName: "Valeria",
-      lastName: "Keysel",
+      firstName: "Flossie",
+      lastName: "Camings",
     },
     provider: "AOK",
-    insuranceType: "LIABILITY",
-    status: "ACTIVE",
-    startDate: "2016-04-14T02:53:58Z",
+    insuranceType: "HEALTH",
+    status: "PENDING",
+    startDate: "2020-05-02T05:53:46Z",
   },
 ];
-// assign unique id to each item
-const mockDataWithId = mockData.map((item, idx) => ({
-  ...item,
-  id: (idx + 1).toString(),
-}));
 
 const handlers = [
-  // Handles a GET /policies request
   rest.get("http://localhost:4000/policies", (req, res, ctx) => {
     const keyword = req.url.searchParams.get("search");
-    const filteredMockData = mockDataWithId.filter(
-      // TODO: filter need to work with other fields too
-      (item) => item.provider === keyword
-    );
-    // return a mocked policy list
-    return res(
-      ctx.status(200),
-      ctx.json(keyword ? filteredMockData : mockDataWithId)
-    );
+    if (keyword === "aok") {
+      return res(ctx.status(200), ctx.json(mockDataAok));
+    }
+    return res(ctx.status(200), ctx.json(mockData));
   }),
 ];
 
@@ -247,16 +192,20 @@ beforeEach(async () => {
   await waitForElementToBeRemoved(() => screen.getByText("loading..."));
 });
 
-// Reset any request handlers that we may add during the tests,
-// so they don't affect other tests.
+// Reset any request handlers that we may add during the tests
 afterEach(() => server.resetHandlers());
 
 // Clean up after the tests are finished.
 afterAll(() => server.close());
 
 describe("Show only ACTIVE and PENDING policies", () => {
+  test("renders correct number of rows", async () => {
+    const items = await screen.findAllByRole("row");
+    expect(items.length).toBe(9); // header row included
+  });
+
   test("renders active policies", async () => {
-    const items = await screen.findAllByText("ACTIVE");
+    const items = await screen.findAllByText("ACTIVE"); // TODO: change to role
     items.map((item) => expect(item).toBeInTheDocument());
   });
 
@@ -277,13 +226,23 @@ describe("Show only ACTIVE and PENDING policies", () => {
 });
 
 describe("As a user, I want to be able to search for policies using any of the text fields displayed on the table.", () => {
+  const user = userEvent.setup();
+
   test("When a search filter is applied, I want to see the filtered information on the same table.", async () => {
-    const user = userEvent.setup();
-    await user.type(screen.getByRole("textbox", { name: "" }), "AOK");
+    await user.type(screen.getByRole("textbox", { name: "" }), "aok");
     await user.click(screen.getByRole("button", { name: "Search" }));
     const items = await screen.findAllByRole("row");
-    expect(items.length).toBe(4); // header row included
+    expect(items.length).toBe(3); // header row included
   });
-  // test("When a search filter is applied, I want to be able to clear the current search filter, this action will display the original information. Clearing the search should return the table to its original state", () => {});
+  // test search filter for status
+  // test search filter for type
+  // test search filter for client
+
+  test("When a search filter is applied, I want to be able to clear the current search filter, this action will display the original information. Clearing the search should return the table to its original state", () => {
+    // await user.type(screen.getByRole("textbox", { name: "" }), "aok");
+    // await user.click(screen.getByRole("button", { name: "Search" }));
+    // const items = await screen.findAllByRole("row");
+    // expect(items.length).toBe(3); // header row included
+  });
   // test("Do not display any results if there are no matches", async () => {});
 });
