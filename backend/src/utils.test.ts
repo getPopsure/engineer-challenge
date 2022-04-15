c2ab59385e1ca2313cb5339d799b74bba352bf48
ruizhen88 <yejiin.jung@gmail.com>
2022-04-15 18:23:04 +0800

    test: partial string enum match util func test

--- backend/src/utils.test.ts
+++ backend/src/utils.test.ts
@@ -2,13 +2,18 @@ import { InsuranceType, PolicyStatus } from "@prisma/client";
 import { getMatchingEnumValueByString } from "./utils";
 
 describe("get matching enum value by string", () => {
-  test("single match should return single result", () => {
+  test("partial match should return result", () => {
     const result = getMatchingEnumValueByString(InsuranceType, "lia");
     expect(result).toStrictEqual(["LIABILITY"]);
   });
 
+  test("single match should return single result", () => {
+    const result = getMatchingEnumValueByString(InsuranceType, "liability");
+    expect(result).toStrictEqual(["LIABILITY"]);
+  });
+
   test("multiple matches should return multiple results", () => {
-    const result = getMatchingEnumValueByString(InsuranceType, "e");
+    const result = getMatchingEnumValueByString(InsuranceType, "h");
     expect(result).toStrictEqual(["HOUSEHOLD", "HEALTH"]);
   });
 
