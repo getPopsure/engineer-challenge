62ff1df40a13f10078a51494bf1d154103dae8c4
Dima <dima.delmar@gmail.com>
2022-05-21 14:25:00 +0200

    Remove mock from index.test.ts

--- backend/test/integration/api/index.test.ts
+++ backend/test/integration/api/index.test.ts
@@ -2,22 +2,15 @@ import {describe, expect, test} from "@jest/globals"
 import {app, server} from "../../../src"
 import request from "supertest"
 import {prismaMock} from "../../singleton"
-import {InsuranceType, Policy, PolicyStatus} from "@prisma/client"
+import {mockReset} from "jest-mock-extended"
+
+beforeEach(() => {
+  mockReset(prismaMock)
+})
+
 
 describe("Test the root endpoint", function () {
   test("It should response the GET method", done => {
-    const policy: Policy = {
-      id: "1",
-      customerId: "1",
-      provider: "google",
-      insuranceType: InsuranceType.HEALTH,
-      status: PolicyStatus.ACTIVE,
-      startDate: new Date(),
-      endDate: null,
-      createdAt: new Date()
-    }
-
-    prismaMock.policy.create.mockResolvedValue(policy)
     request(app)
       .get("/")
       .then((response: { statusCode: any }) => {
