97f3231c7688f1323ff2617fd9b73f3351797a65
Ana Tarrisse <at@demodern.de>
2022-10-03 21:58:57 +0200

    feat: create Checkbox component

---
+++ frontend/src/components/Checkbox.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import slugify from "../utils/slugify";
+
+type TCheckbox = React.HTMLAttributes<HTMLDivElement>;
+
+interface IProps {
+  label: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+}
+
+const Checkbox: React.FC<TCheckbox & IProps> = ({ label, onChange }) => (
+  <div>
+    <input
+      className=""
+      id={slugify(label)}
+      type="checkbox"
+      onChange={onChange}
+      value={label}
+    />
+    <label
+      className=""
+      htmlFor={slugify(label)}
+    >
+      {label}
+    </label>
+  </div>
+);
+
+export default Checkbox;
