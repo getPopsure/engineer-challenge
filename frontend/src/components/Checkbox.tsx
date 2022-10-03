4036dc24541062bd6f0fcd5e72e99caef4e81261
Ana Tarrisse <at@demodern.de>
2022-10-03 22:20:03 +0200

    refactor: controlled Checkbox

--- frontend/src/components/Checkbox.tsx
+++ frontend/src/components/Checkbox.tsx
@@ -4,18 +4,20 @@ import slugify from "../utils/slugify";
 type TCheckbox = React.HTMLAttributes<HTMLDivElement>;
 
 interface IProps {
+  checked: boolean;
   label: string;
   onChange: React.ChangeEventHandler<HTMLInputElement>;
 }
 
-const Checkbox: React.FC<TCheckbox & IProps> = ({ label, onChange }) => (
+const Checkbox: React.FC<TCheckbox & IProps> = ({ label, checked, onChange }) => (
   <div>
     <input
       className=""
+      checked={checked}
       id={slugify(label)}
       type="checkbox"
-      onChange={onChange}
       value={label}
+      onChange={onChange}
     />
     <label
       className=""
