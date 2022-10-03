4d460d20682da702aa4c06a9cdab1126db9d43fc
Ana Tarrisse <at@demodern.de>
2022-10-03 20:15:53 +0200

    feat: create FilterButton component

---
+++ frontend/src/components/FilterButton.tsx
@@ -0,0 +1,16 @@
+type TFilter = React.HTMLAttributes<HTMLButtonElement>;
+
+interface IProps {
+  callback: () => void;
+}
+
+const FilterButton: React.FC<TFilter & IProps> = ({ callback }) => (
+  <button
+    className="border bg-primary-500 font-bold py-3 px-6 rounded-md text-white hover:bg-primary-700"
+    onClick={callback}
+  >
+    Add Filter
+  </button>
+);
+
+export default FilterButton;
