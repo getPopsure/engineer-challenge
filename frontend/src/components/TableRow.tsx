c56c8369f0e89fe1fa8b00e8f6daec975044869b
Ana Tarrisse <at@demodern.de>
2022-10-04 18:26:07 +0200

    feat: click on cell add filters

--- frontend/src/components/TableRow.tsx
+++ frontend/src/components/TableRow.tsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import Badge from "./Badge";
 
 import { Policy } from "../types";
+import { Context } from "../context";
 
 interface IProps {
   policy: Policy;
 }
 
 const TableRow: React.FC<IProps> = ({ policy }) => {
+  const { addFilter } = useContext(Context);
+
   return (
     <tr className="border-b">
       <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{policy.id}</td>
@@ -16,13 +19,25 @@ const TableRow: React.FC<IProps> = ({ policy }) => {
         {policy.customer.firstName} {policy.customer.lastName}
       </td>
       <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-        {policy.provider}
+        <button
+          className="hover:underline hover:underline-offset-4"
+          onClick={() => addFilter("provider", policy.provider)}>
+          {policy.provider}
+        </button>
       </td>
       <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap capitalize">
-        {policy.insuranceType.toLowerCase()}
+        <button
+          className="capitalize hover:underline hover:underline-offset-4"
+          onClick={() => addFilter("type", policy.insuranceType)}>
+          {policy.insuranceType.toLowerCase()}
+        </button>
       </td>
       <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-        <Badge status={policy.status} />
+        <button
+          className="hover:underline hover:underline-offset-4"
+          onClick={() => addFilter("status", policy.status)}>
+          <Badge status={policy.status} />
+        </button>
       </td>
     </tr>
   )
