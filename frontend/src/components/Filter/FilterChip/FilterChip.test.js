102bd3fc8247b579f1a5f0df4257139215036686
Ana Tarrisse <at@demodern.de>
2022-10-11 22:28:41 +0200

    feat(fe): tests for FilterChip

--- frontend/src/components/Filter/FilterChip/FilterChip.test.js
+++ frontend/src/components/Filter/FilterChip/FilterChip.test.js
@@ -1,9 +1,25 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { Context } from "../../../context";
+
+import { contextValue } from "../../../tests/mocks";
+import FilterChip from './FilterChip';
 
 describe("FilterChip", () => {
-  test("click on button removes filter", () => {
+  beforeEach(() => {
+    render(
+      <Context.Provider value={contextValue}>
+        <FilterChip filterKey='provider' value='provider' />
+      </Context.Provider>)
+  });
 
+  test("renders chip", () => {
+    expect(screen.getByText(/provider/i)).toBeInTheDocument();
+  });
+
+  test("click on button calls removes filter", () => {
+    const button = screen.getByRole("button");
+    fireEvent.click(button);
+    expect(contextValue.removeFilter).toHaveBeenCalled();
   })
-})
+});
