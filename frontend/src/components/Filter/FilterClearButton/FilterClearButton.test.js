ed2c0d7dac9a8320e78352638bb594417c5e3c86
Ana Tarrisse <at@demodern.de>
2022-10-11 22:33:15 +0200

    feat(fe): tests for FilterClearButton

--- frontend/src/components/Filter/FilterClearButton/FilterClearButton.test.js
+++ frontend/src/components/Filter/FilterClearButton/FilterClearButton.test.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { Context } from "../../../context";
+import { contextValue } from "../../../tests/mocks";
+import FilterClearButton from "./FilterClearButton";
 
 describe("FilterClearButton", () => {
-  test("click clears all filters", () => {
-    // 
+  test("click on button calls clear filter", () => {
+    render(
+      <Context.Provider value={contextValue}>
+        <FilterClearButton />
+      </Context.Provider>)
+    const button = screen.getByRole("button");
+    fireEvent.click(button);
+    expect(contextValue.clearAllFilters).toHaveBeenCalled();
   })
 })
