d904596c6d350a6d7b305b5a16494653523d8048
Ana Tarrisse <at@demodern.de>
2022-10-11 22:35:25 +0200

    feat(fe): tests for TableFilter

--- frontend/src/components/Table/TableFilter/TableFilter.test.js
+++ frontend/src/components/Table/TableFilter/TableFilter.test.js
@@ -1,18 +1,32 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, within } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { Context } from "../../../context";
+
+import TableFilter from './TableFilter';
+
+import { contextValue } from "../../../tests/mocks";
 
 describe("TableFilter", () => {
+  beforeEach(() => {
+    render(
+      <Context.Provider value={contextValue}>
+        <TableFilter />
+      </Context.Provider>
+    )
+  });
   test("renders name search input", () => {
-    // 
+    expect(screen.getByRole("textbox")).toBeInTheDocument();
   })
   test("renders providers filter", () => {
-    // 
+    const dropdown = screen.getByTestId("filter-dropdown-provider");
+    expect(within(dropdown).getByText(/provider/i)).toBeInTheDocument();
   })
   test("renders insurance types filter", () => {
-    // 
+    const dropdown = screen.getByTestId("filter-dropdown-insuranceType");
+    expect(within(dropdown).getByText(/insurance type/i)).toBeInTheDocument();
   })
   test("renders status filter", () => {
-    // 
+    const dropdown = screen.getByTestId("filter-dropdown-status");
+    expect(within(dropdown).getByText(/status/i)).toBeInTheDocument();
   })
 })
