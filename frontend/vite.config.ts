88b126bb1d49f38c4be8c8c9c48b4c7aed329787
Davide <davide.cuccato5@gmail.com>
2022-10-18 16:21:55 +0200

    Migrated from CRA to Vite

---
+++ frontend/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig((configEnv) => {
+  const isDevelopment = configEnv.mode === "development";
+
+  return {
+    plugins: [react(), tsconfigPaths()],
+    css: {
+      modules: {
+        generateScopedName: isDevelopment
+          ? "[name]__[local]__[hash:base64:5]"
+          : "[hash:base64:5]",
+      },
+    },
+  };
+});
